93d96e8a44caaa50f2b9f57f2bab76175d8c9343
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2017-03-10 13:20:40 +0100

    dir structure reorg

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject archiet.clj3manchess.engine "0.1.0-SNAPSHOT"
+(defproject clj3manchess "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.8.0"]])
