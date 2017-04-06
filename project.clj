a328d9087638ecd972036cc3b49bb058bb084718
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2017-04-06 19:27:21 +0200

    clojurescript

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
 (defproject clj3manchess "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+                 [org.clojure/clojurescript "1.9.494"]]
+  :plugins [[lein-cljsbuild "1.1.5"]
+            [lein-figwheel ["0.5.9"]]]
+  :source-paths ["src"]
   :test-paths ["test"])
