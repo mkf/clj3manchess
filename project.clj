17caf20319fa3f752f60d53bd6fa38b880fefbf7
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2017-04-06 23:57:58 +0200

    project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,18 @@
 (defproject clj3manchess "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.494"]]
   :plugins [[lein-cljsbuild "1.1.5"]
-            [lein-figwheel ["0.5.9"]]]
+            [lein-figwheel "0.5.9"]]
   :source-paths ["src"]
-  :test-paths ["test"])
+  :test-paths ["test"]
+  :cljsbuild
+  {:builds
+   {:test {:source-paths ["src" "test"]
+           :incremental true
+           :compiler {:output-to "target/main.js"
+                      :output-dir "target"
+                      :optimizations :none
+                      :pretty-print true}}}})
+
