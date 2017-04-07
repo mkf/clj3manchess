85d4bd8789b8a5415428dbd3cd06f4033d92f071
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2017-04-07 02:35:22 +0200

    changed :require in vector_test

--- test/clj3manchess/engine/vector_test.cljs
+++ test/clj3manchess/engine/vector_test.cljs
@@ -1,6 +1,6 @@
 (ns clj3manchess.engine.vector-test
-  (:require  [clojure.test :as t]
-             [clj3manchess.engine.vectors :as v]))
+  (:require [cljs.test :refer-macros [with-test is]]
+            [clj3manchess.engine.vectors :as v]))
 
 (with-test
   (is (= {:inward true} (first (v/units {:inward true :abs 3} 4)))))
