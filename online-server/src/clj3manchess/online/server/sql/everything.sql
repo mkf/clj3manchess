1de40f3a879bde18730f3b54d37f42eb8534f5aa
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2017-04-20 20:11:16 +0200

    Quite a lot of SQL

--- online-server/src/clj3manchess/online/server/sql/everything.sql
+++ online-server/src/clj3manchess/online/server/sql/everything.sql
@@ -1,9 +1,7 @@
--- :name create-state-table
--- :command :execute
--- :result :raw
+-- :name create-st-table :! :raw
 create table if not exists c3mst (
        id bigint auto_increment primary key,
-       board binary(144), --null is newgame; input: best in hex; zeros are empty squares
+       board binary(144) not null,
        moats tinyint not null, -- _ _ _ _ _ W G B
        movesnext tinyint not null, -- w1 g2 b3
        castling tinyint not null, -- _ _ WK WQ GK GQ BK BQ
@@ -17,12 +15,107 @@ create table if not exists c3mst (
        )
 ) ENGINE = InnoDB;
 
--- :name create-gp-table
--- :command :execute
--- :result :raw
+-- :name get-state-by-id :? :1
+select * from c3mst
+where id = :id
+
+-- :name create-gp-table :! :raw
 create table if not exists c3mgp (
        id bigint auto_increment primary key,
-       state bigint,
+       state bigint not null,
        created timestamp default current_timestamp,
        constraint foreign key (state) references c3mst (id) on update restrict
 ) ENGINE = InnoDB;
+
+-- :name get-just-gp-by-id :? :1
+select * from c3mgp
+where id = :id;
+
+-- :name get-gp-by-id :? :1
+select gp.id as id, gp.created, gp.state,
+                st.board, st.moats, st.movesnext, st.castling, st.enpassant_prev, st.enpassant_last,
+                st.halfmoveclock, st.fullmovenumber, st.alive
+from c3mgp gp
+join c3mst st on st.id = gp.state;
+
+-- :name insert-just-new-gp! :! :1
+insert into c3mgp (state) values(:state) returning id;
+
+-- :name insert-new-gp! :! :1
+begin;
+insert into c3mst (board,
+                   moats,
+                   movesnext,
+                   castling,
+                   enpassant_prev,
+                   enpassant_last,
+                   halfmoveclock,
+                   fullmovenumber,
+                   alive)
+values (unhex(:board),
+        :moats,
+        :movesnext,
+        :castling,
+        :enpassant_prev,
+        :enpassant_last,
+        :halfmoveclock,
+        :fullmovenumber,
+        :alive)
+on duplicate key update id=last_insert_id(id);
+insert into c3mgp (state) values(last_insert_id()) returning id;
+commit;
+
+-- :name create-mv-table :! :raw
+create table if not exists c3mmv (
+       id bigint auto_increment primary key,
+       fromto binary(4) not null,
+       promotion tinyint,
+       beforegame bigint not null,
+       aftergame bigint, --not null
+       -- who
+) ENGINE = InnoDB;
+
+-- :name get-mv-by-id-with-after :! :1
+select mv.id, mv.fromto, mv.promotion, mv.beforegame, mv.aftergame,
+       be.created as b_created, be.state as b_state,
+       af.created as a_created, af.state as a_state,
+       bs.board as b_board, bs.moats as b_moats, bs.movesnext as b_movesnext,
+       bs.castling as b_castling, bs.enpassant_prev as b_enpassant_prev, bs.enpassant_last as b_enpassant_last,
+       bs.halfmoveclock as b_halfmoveclock, bs.fullmovenumber as b_fullmovenumber, bs.alive as b_alive,
+       sa.board as a_board, sa.moats as a_moats, sa.movesnext as a_movesnext,
+       sa.castling as a_castling, sa.enpassant_prev as a_enpassant_prev, sa.enpassant_last as a_enpassant_last,
+       sa.halfmoveclock as a_halfmoveclock, sa.fullmovenumber as a_fullmovenumber, sa.alive as a_alive,
+from c3mmv mv
+join c3mgp be on mv.beforegame = be.id
+left join c3mgp af on mv.aftergame = be.id
+join c3mst bs on be.state = bs.id
+left join c3mst sa on af.state = sa.id;
+
+-- :name insert-just-new-move :! :1
+insert into c3mmv (fromto, promotion, beforegame, aftergame)
+values (:fromto, :prom, :beforegame, :aftergame) returning id;
+
+-- :name insert-new-move! :! :1
+begin;
+insert into c3mst (board,
+                   moats,
+                   movesnext,
+                   castling,
+                   enpassant_prev,
+                   enpassant_last,
+                   halfmoveclock,
+                   fullmovenumber,
+                   alive)
+values (unhex(:board),
+        :moats,
+        :movesnext,
+        :castling,
+        :enpassant_prev,
+        :enpassant_last,
+        :halfmoveclock,
+        :fullmovenumber,
+        :alive)
+on duplicate key update id=last_insert_id(id);
+insert into c3mgp (state) values(last_insert_id());
+insert into c3mmv (fromto, promotion, beforegame, aftergame)
+values (:fromto, :prom, :beforegame, last_insert_id()) returning id;
