3f699353f0e7104a05361c04dd65fa676026c6e4
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2017-04-21 17:24:33 +0200

    Working on state-resource

--- online-server/src/clj3manchess/online/server.clj
+++ online-server/src/clj3manchess/online/server.clj
@@ -4,10 +4,18 @@
              [compojure.core :refer [defroutes ANY context GET POST]]
              [clj3manchess.online.server.mysql :as d]))
 
+(defresource state-resource [id]
+  :allowed-methods [:get]
+  :exists? (fn [_]
+             (when-let [e (d/get-state-by-id id)] {::state e}))
+  :available-media-types ["application/edn"]
+  :handle-ok #(prn-str (::state %)))
+
 (defroutes app
   (ANY "/" [] (resource))
   (context "/api" []
            (defroutes api-routes
+             (ANY ["/state/:id{[0-9]+}"] [id] (state-resource id))
              (ANY "/" [] (resource)))))
 
 (def handler (-> app wrap-params))
