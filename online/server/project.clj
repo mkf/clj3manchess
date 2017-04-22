4de6a0575db1d38fc18a6794a6816c7457c0eec9
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2017-04-22 20:06:19 +0200

    server&main project.clj

--- online/server/project.clj
+++ online/server/project.clj
@@ -1,9 +1,10 @@
-(defproject clj3manchess.online.server "0.2.0-SNAPSHOT"
+(defproject clj3manchess/online-server "0.2.0-SNAPSHOT"
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler clj3manchess.online.server/handler
          :init clj3manchess.online.server/init}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj3manchess "0.2.0-SNAPSHOT"]
+                 [clj3manchess/engine "0.3.0-SNAPSHOT"]
+                 [clj3manchess/online-core "0.3.0-SNAPSHOT"]
                  [liberator "0.14.1"] ;;not anymore
                  [compojure "1.5.2"]
                  [metosin/compojure-api "1.1.10"]
